59fbc6c0e1a67102cdf1098cf2d7c732e808e6f4
Vinicius Palmer <47270992+ViniciusPalmer@users.noreply.github.com>
2026-05-05 19:27:53 -0300

    refactor(Home): migrate from styled-components to Tailwind

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,21 @@
-import { HomeContainer, HomeContent, GoogleImg } from "./styles";
-
-import GoogleLogo from "../../assets/google_icon.png";
-
 import { Footer } from "../../components/Footer";
 import { HomeHeader } from "./components/HomeHeader";
 import { SearchMenuBar } from "../../components/SearchMenuBar";
 import { SeoMetadata } from "../../components/SeoMetadata";
 
+import GoogleLogo from "../../assets/google_icon.png";
+
 export function Home() {
   return (
-    <HomeContainer>
+    <main className="w-screen h-screen flex flex-col items-center">
       <SeoMetadata title="Animal Search" description="Search for various animals by type or name" />
       <HomeHeader />
-      <HomeContent>
-        <h1 style={{ position: "absolute", width: 1, height: 1, padding: 0, margin: -1, overflow: "hidden", clip: "rect(0,0,0,0)", border: 0 }}>
-          Animal Search Home
-        </h1>
-        <GoogleImg src={GoogleLogo} alt="" />
+      <div className="w-[40vw] flex flex-col items-center justify-around fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[80vw]">
+        <h1 className="sr-only">Animal Search Home</h1>
+        <img className="w-3/4 mb-6" src={GoogleLogo} alt="" />
         <SearchMenuBar />
-      </HomeContent>
+      </div>
       <Footer />
-    </HomeContainer>
+    </main>
   );
 }
