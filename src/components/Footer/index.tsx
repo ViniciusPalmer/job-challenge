94892aa5440c5c0dcf8597be4a7ff0cc892ba017
Vinicius Palmer <47270992+ViniciusPalmer@users.noreply.github.com>
2026-05-05 19:06:38 -0300

    refactor(Footer): migrate from styled-components to Tailwind

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,9 @@
-import { FooterContainer, FooterText } from "./styles";
-
 export function Footer() {
-  const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear()
   return (
-    <FooterContainer>
-      <FooterText>© Google {currentYear}</FooterText>
-      <FooterText>version 0.1.0</FooterText>
-    </FooterContainer>
-  );
+    <footer className="flex w-full items-center justify-between p-6 bg-gray-50 fixed bottom-0 left-0">
+      <span className="text-sm text-gray-300">© Google {currentYear}</span>
+      <span className="text-sm text-gray-300">version 0.1.0</span>
+    </footer>
+  )
 }
