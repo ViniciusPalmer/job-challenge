a637070c995c688c17c1dfb44e4f8b6828fef449
Vinicius Palmer <47270992+ViniciusPalmer@users.noreply.github.com>
2026-05-05 19:12:02 -0300

    refactor(ResultCard): migrate from styled-components to Tailwind

--- src/pages/Results/components/ResultCard/index.tsx
+++ src/pages/Results/components/ResultCard/index.tsx
@@ -1,10 +1,3 @@
-import {
-  ResultCardContainer,
-  URLText,
-  LinkText,
-  DescriptionText,
-} from "./styles";
-
 import { IAnimal } from "../../../../types/animal";
 
 interface IResultCard {
@@ -18,10 +11,17 @@ export function ResultCard({ animal, setAnimal }: IResultCard) {
   }
 
   return (
-    <ResultCardContainer>
-      <URLText>{animal.url}</URLText>
-      <LinkText onClick={selectAnimal}>{animal.title}</LinkText>
-      <DescriptionText>{animal.description}</DescriptionText>
-    </ResultCardContainer>
+    <article className="flex flex-col max-h-[200px] items-start mb-7">
+      <span className="text-xs text-gray-300 mb-2">{animal.url}</span>
+      <button
+        onClick={selectAnimal}
+        className="text-2xl text-blue-300 mb-3 bg-transparent border-none cursor-pointer hover:brightness-60 transition-all"
+      >
+        {animal.title}
+      </button>
+      <p className="text-base text-gray-300 mb-3 hover:brightness-60 transition-all">
+        {animal.description}
+      </p>
+    </article>
   );
 }
