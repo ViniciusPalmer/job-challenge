4845da05a4f2cc22e70941f91d8f1569565efafa
Vinicius Palmer <47270992+ViniciusPalmer@users.noreply.github.com>
2026-05-05 19:33:04 -0300

    chore: remove styled-components and ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
 
@@ -9,15 +6,12 @@ import { AnimalsDataProvider } from "./contexts/animalData";
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <SearchInputProvider>
-          <AnimalsDataProvider>
-            <Router />
-          </AnimalsDataProvider>
-        </SearchInputProvider>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <SearchInputProvider>
+        <AnimalsDataProvider>
+          <Router />
+        </AnimalsDataProvider>
+      </SearchInputProvider>
+    </BrowserRouter>
   );
 }
