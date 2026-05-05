a4ac310fe4e2d2b40a7b37089ba68ac9b74a1ba4
Vinicius Palmer <47270992+ViniciusPalmer@users.noreply.github.com>
2026-05-05 19:19:52 -0300

    refactor(HomeHeader): migrate from styled-components to Tailwind

--- src/pages/Home/components/HomeHeader/index.tsx
+++ src/pages/Home/components/HomeHeader/index.tsx
@@ -1,18 +1,16 @@
-import { HeaderContainer, HeaderText, IconsContainer } from "./styles";
-
 import googleAppsIcon from "../../../../assets/google_apps_icon.svg";
 import userAvatar from "../../../../assets/user_avatar.svg";
 
 export function HomeHeader() {
   return (
-    <HeaderContainer>
-      <HeaderText>
+    <header className="flex w-full items-center justify-between p-6 border-b border-gray-150">
+      <h1 className="text-xl font-light md:text-base">
         <b>Agile Content</b> Frontend Test
-      </HeaderText>
-      <IconsContainer>
-        <img src={googleAppsIcon} alt="Google Apps" />
-        <img src={userAvatar} alt="Female Avatar" />
-      </IconsContainer>
-    </HeaderContainer>
+      </h1>
+      <div className="flex flex-row p-2 w-[110px] h-[60px]">
+        <img className="w-6 h-6" src={googleAppsIcon} alt="Google Apps" />
+        <img className="w-6 h-6 ml-4" src={userAvatar} alt="Female Avatar" />
+      </div>
+    </header>
   );
 }
